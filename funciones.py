b3d78f5a415bc1873b977a15859c254dc36f22fe
PabloxDxD <99362268+PabloxDxD@users.noreply.github.com>
2023-03-21 09:12:06 -0600

    commit

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def perimetro():
+    lado = input(float("Ingresa un numero para el primer lado: "))
+    lado2= input(float("Ingresa un numero para el segundo lado: "))
+    lado3 = input(float("Ingresa un numero para el tercer lado: "))
+
+    Perimetro = lado + lado2 + lado3
+    print("El resultado de su perimetro es: ",Perimetro)
+
---
+++ main.py
@@ -0,0 +1,13 @@
+import funciones as fun
+
+print('Bienvenido querid@ usuario :)')
+print('''Que operacion deseas realizar
+    1. Perimetro
+    2. Area
+    3. Clasificacion lado
+    4. Clasificacion angulo''')
+opcion = input()
+
+if opcion == '1':
+    fun.perimetro()
+
